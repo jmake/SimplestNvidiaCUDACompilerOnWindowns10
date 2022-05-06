6175d4e5fba2f399d64b17965b156df5c5b3f5db
jmake <zvl_2006@hotmail.com>
2022-05-06 20:26:31 +0200

    Update simplest.ps1

--- Actions/NVCC/simplest.ps1
+++ Actions/NVCC/simplest.ps1
@@ -1,4 +1,38 @@
 
+function SETUP_CUDART 
+{
+      echo "SETUP_CUDART ..."
+      
+      $url="https://developer.download.nvidia.com/compute/cuda/redist/cuda_cudart/windows-x86_64/cuda_cudart-windows-x86_64-11.5.50-archive.zip"
+      $output="cudart.zip"
+      Invoke-WebRequest -Uri $url -OutFile $output
+      Expand-Archive $output
+      cd cudart\cuda_cudart-windows-x86_64-11.5.50-archive
+      ls 
+      
+      [Environment]::SetEnvironmentVariable("CUDART_PATH","${PWD}")
+      cd ${HOME}
+}
+
+
+function SETUP_NVCC
+{ 
+      echo "SETUP_NVCC ..."
+      
+      $url="https://developer.download.nvidia.com/compute/cuda/redist/cuda_nvcc/windows-x86_64/cuda_nvcc-windows-x86_64-11.5.50-archive.zip"
+      $output="nvcc.zip"
+      Invoke-WebRequest -Uri $url -OutFile $output
+      Expand-Archive $output
+      cd nvcc
+      cd cuda_nvcc-windows-x86_64-11.5.50-archive
+      cd bin
+      .\nvcc.exe --version 
+      
+      [Environment]::SetEnvironmentVariable("NVCC_PATH","${PWD}")
+      [Environment]::SetEnvironmentVariable("NVCC","${PWD}\nvcc.exe")
+      cd ${HOME}
+}
+
 function CL_SETUP
 {
   $VSWHERE="C:\ProgramData\Chocolatey\bin\vswhere.exe"
@@ -18,10 +52,18 @@ function CL_SETUP
   
   cl.exe 
 }
+
+
+$RELEASE=11.5.50
+SETUP_CUDART
+SETUP_NVCC
 CL_SETUP
 
 New-Item -ItemType Directory BUILD
 Set-Location BUILD
 
+
+$env:CUDART_PATH
+$env:NVCC
 &$env:NVCC --version 
 
