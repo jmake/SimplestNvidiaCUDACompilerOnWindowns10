9c921282b4b9eef2bf0c7efa77ab446ea704f620
jmake <zvl_2006@hotmail.com>
2022-05-06 19:42:37 +0200

    Create simplest.cu

---
+++ Actions/NVCC/simplest.cu
@@ -0,0 +1,12 @@
+__global__
+void cuda_hello()
+{
+  printf(">> [smallest] Hello from GPU!\n");
+}
+
+int main()
+{
+    printf(">> [smallest] Hello from CPU!\n");   
+    cuda_hello<<<1,1>>>();
+    return 0;
+}
