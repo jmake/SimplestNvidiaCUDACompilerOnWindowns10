c858735cc502cc4add5ef504680db694ede84fcf
jmake <zvl_2006@hotmail.com>
2022-05-06 19:47:09 +0200

    Update simplest.ps1

--- Actions/NVCC/simplest.ps1
+++ Actions/NVCC/simplest.ps1
@@ -1,19 +1,24 @@
-$VSWHERE="C:\ProgramData\Chocolatey\bin\vswhere.exe"
 
-$VSTOOLS = &($VSWHERE) -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
-Write-Output "[VSTOOLS]:'$VSTOOLS' "
-
-if($VSTOOLS) 
+function CL_SETUP
 {
-  $VSTOOLS = join-path $VSTOOLS 'Common7\Tools\vsdevcmd.bat'
-  if (test-path $VSTOOLS) 
+  $VSWHERE="C:\ProgramData\Chocolatey\bin\vswhere.exe"
+
+  $VSTOOLS = &($VSWHERE) -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+  Write-Output "[VSTOOLS]:'$VSTOOLS' "
+
+  if($VSTOOLS) 
   {
-    cmd /s /c """$VSTOOLS"" $args && set" | where { $_ -match '(\w+)=(.*)' } | 
-    foreach{$null = new-item -force -path "Env:\$($Matches[1])" -value $Matches[2] }
+    $VSTOOLS = join-path $VSTOOLS 'Common7\Tools\vsdevcmd.bat'
+    if (test-path $VSTOOLS) 
+    {
+      cmd /s /c """$VSTOOLS"" $args && set" | where { $_ -match '(\w+)=(.*)' } | 
+      foreach{$null = new-item -force -path "Env:\$($Matches[1])" -value $Matches[2] }
+    }
   }
+  
+  cl.exe 
 }
-           
-cl.exe 
+CL_SETUP
 
 New-Item -ItemType Directory BUILD
 Set-Location BUILD
