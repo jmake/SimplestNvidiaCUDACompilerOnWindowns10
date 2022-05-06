2cd4e6278115f07a4e05b2edf36f1c51007c4469
jmake <zvl_2006@hotmail.com>
2022-05-06 23:48:45 +0200

    Update simplest.ps1

--- Actions/NVCC/simplest.ps1
+++ Actions/NVCC/simplest.ps1
@@ -83,7 +83,7 @@ rm simplest.exe
 
 
 ## 2.C. FANCIEST (https://www.collinsdictionary.com/dictionary/english/fanciest) 
-cmake.exe .. -G Ninja -DCMAKE_CUDA_ARCHITECTURES="native"
+cmake.exe .. -G Ninja -DCMAKE_CUDA_ARCHITECTURES="all"
 
 #ninja.exe
 #ctest.exe
