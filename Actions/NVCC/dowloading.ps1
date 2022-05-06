b85e5c1773df0662be764267ed74cde35995b26f
jmake <zvl_2006@hotmail.com>
2022-05-06 20:12:54 +0200

    Update dowloading.ps1

--- Actions/NVCC/dowloading.ps1
+++ Actions/NVCC/dowloading.ps1
@@ -10,6 +10,8 @@ function SETUP_CUDART
       Expand-Archive $output
       cd cudart\cuda_cudart-windows-x86_64-11.5.50-archive
       ls 
+
+      cd ${HOME}
 }
 
 function SETUP_NVCC
@@ -24,7 +26,7 @@ function SETUP_NVCC
       cd cuda_nvcc-windows-x86_64-11.5.50-archive
       cd bin
       .\nvcc.exe --version 
-      [Environment]::SetEnvironmentVariable("NVCC",${PWD})
+      [Environment]::SetEnvironmentVariable("NVCC","${PWD}\nvcc.exe")
       
       cd ${HOME}
 }
