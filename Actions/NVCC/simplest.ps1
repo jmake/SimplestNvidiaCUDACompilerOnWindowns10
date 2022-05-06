954575907116c61897ae969c9dd6985ffff8333c
jmake <zvl_2006@hotmail.com>
2022-05-06 11:03:29 +0200

    Create simplest.ps1

---
+++ Actions/NVCC/simplest.ps1
@@ -0,0 +1,23 @@
+$VSWHERE="C:\ProgramData\Chocolatey\bin\vswhere.exe"
+
+$VSTOOLS = &($VSWHERE) -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+Write-Output "[VSTOOLS]:'$VSTOOLS' "
+
+if($VSTOOLS) 
+{
+  $VSTOOLS = join-path $VSTOOLS 'Common7\Tools\vsdevcmd.bat'
+  if (test-path $VSTOOLS) 
+  {
+    cmd /s /c """$VSTOOLS"" $args && set" | where { $_ -match '(\w+)=(.*)' } | 
+    foreach{$null = new-item -force -path "Env:\$($Matches[1])" -value $Matches[2] }
+  }
+}
+           
+cl.exe 
+
+wget 
+
+
+New-Item -ItemType Directory BUILD
+Set-Location BUILD
+
