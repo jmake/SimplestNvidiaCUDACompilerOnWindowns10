185e06af45a41f468c7da8fc9d669e8889d7a3ad
jmake <zvl_2006@hotmail.com>
2022-05-07 12:03:13 +0200

    Update simplest.ps1

--- Actions/NVCC/simplest.ps1
+++ Actions/NVCC/simplest.ps1
@@ -3,37 +3,58 @@ function CUDART_SETUP
 {
       echo "CUDART_SETUP ..."
       
+      ## 1.0 
       $url="https://developer.download.nvidia.com/compute/cuda/redist/cuda_cudart/windows-x86_64/cuda_cudart-windows-x86_64-11.5.50-archive.zip"
       $output="cudart.zip"
-      Invoke-WebRequest -Uri $url -OutFile $output
-      Expand-Archive $output
-      cd cudart\cuda_cudart-windows-x86_64-11.5.50-archive
-      ls 
       
-      [Environment]::SetEnvironmentVariable("CUDA_PATH","${PWD}")
-      cd ..
-      cd .. 
+      Invoke-WebRequest -Uri $url -OutFile $output
+      Expand-Archive $output -DestinationPath .   
+      
+      $CUDART="cuda_cudart-windows-x86_64-11.5.50-archive"
+      
+      
+      ## 2.0 
+      Set-Location -Path $CUDART
+      Get-ChildItem 
+      
+      $env:CUDART_PATH="$ROOT\$CUDART"
+      $env:CUDART_LIB="env:CUDART_PATH\lib"
+      $env:CUDART_INC="env:CUDART_PATH\include"      
+
+      $env:CUDART_PATH=("env:CUDART_PATH").replace("\","\\")
+      $env:CUDART_LIB=("env:CUDART_LIB").replace("\","\\")
+      $env:CUDART_INC=("env:CUDART_INC").replace("\","\\")
+      
+      Set-Location -Path $ROOT
 }
 
 
 function NVCC_SETUP
 { 
       echo "NVCC_SETUP ..."
-      
+
+      ## 1.0 
       $url="https://developer.download.nvidia.com/compute/cuda/redist/cuda_nvcc/windows-x86_64/cuda_nvcc-windows-x86_64-11.5.50-archive.zip"
       $output="nvcc.zip"
-      Invoke-WebRequest -Uri $url -OutFile $output
-      Expand-Archive $output
-      cd nvcc
-      cd cuda_nvcc-windows-x86_64-11.5.50-archive
-      cd bin
-      .\nvcc.exe --version 
       
-      [Environment]::SetEnvironmentVariable("CUDACXX_PATH","${PWD}")
-      [Environment]::SetEnvironmentVariable("CUDACXX","${PWD}\nvcc.exe")
-      cd ..
-      cd .. 
-      cd ..
+      Invoke-WebRequest -Uri $url -OutFile $output
+      Expand-Archive $output -DestinationPath .   
+      
+      $NVCC="cuda_nvcc-windows-x86_64-11.5.50-archive"
+
+      ## 2.0 
+      Set-Location -Path $NVCC 
+      Set-Location bin 
+      Get-ChildItem 
+      Get-ChildItem nvcc.exe 
+ 
+      $env:NVCC_PATH="$ROOT\$NVCC\bin" 
+      $env:NVCC="$env:NVCC_PATH\nvcc.exe"  
+      
+      $env:NVCC_PATH=("env:NVCC_PATH").replace("\","\\")
+      $env:NVCC=("env:NVCC").replace("\","\\")
+ 
+      Set-Location -Path $ROOT
 }
 
 function CL_SETUP
@@ -56,44 +77,71 @@ function CL_SETUP
   }
   
   cl.exe 
+  cmake.exe --version 
+  ninja.exe --version 
 }
 
-## 1. SETUP ...
+
+function COMPILATION_OBVIOUS 
+{ 
+      &$env:NVCC -o simplest.exe ../simplest.cu -I"$env:CUDART_INC" -L"$env:CUDART_LIB"
+      .\simplest.exe
+      rm simplest.exe 
+}
+
+
+function COMPILATION_NICEST 
+{ 
+      $env:LIBRARIES=" -I ${$env:CUDART_INC}"
+      $env:INCLUDES=" -LIBPATH:{$env:CUDART_LIB} "
+      
+      &$env:NVCC -o simplest.exe ../simplest.cu 
+      .\simplest.exe
+      rm simplest.exe 
+}
+
+
+## 0. 
+$ROOT=Get-Location
 $RELEASE=11.5.50
-CUDART_SETUP
+
+
+## 1. SETUP ...
 NVCC_SETUP
+&$env:NVCC --version. 
+
+CUDART_SETUP
+$env:CUDART_LIB
+$env:CUDART_INC
+
 CL_SETUP
 
-cmake.exe --version 
-ninja.exe --version 
-
-
+## 2. RUNNING ...
 New-Item -ItemType Directory BUILD
 Set-Location BUILD
 
-$env:CUDA_PATH
-$env:CUDACXX
-&$env:CUDACXX --version 
 
-## 2. COMPILATION
-## 2.A. SIMPLEST  
-&$env:CUDACXX -o simplest.exe ../simplest.cu -I"$env:CUDA_PATH/include" -L"$env:CUDA_PATH/lib"
-.\simplest.exe
-rm simplest.exe 
+COMPILATION_OBVIOUS
+COMPILATION_NICEST 
+#COMPILATION_SIMPLEST
+
+
+
+
+#$env:CUDA_PATH
+#$env:CUDACXX
+#&$env:CUDACXX --version 
 
 
 ## 2.C. FANCIEST (https://www.collinsdictionary.com/dictionary/english/fanciest) 
-
-$CMAKE_CUDA_COMPILER=($env:CUDACXX).replace("\","\\")
-$CUDAToolkit_ROOT=($env:CUDA_PATH).replace("\","\\")
-
-echo $CMAKE_CUDA_COMPILER
-cmake.exe .. -G Ninja -DCMAKE_CUDA_COMPILER="$CMAKE_CUDA_COMPILER" -DCUDAToolkit_ROOT="$CUDAToolkit_ROOT"
-
-
+#
+#$CMAKE_CUDA_COMPILER=($env:CUDACXX).replace("\","\\")
+#$CUDAToolkit_ROOT=($env:CUDA_PATH).replace("\","\\")
+#
+#echo $CMAKE_CUDA_COMPILER
+#cmake.exe .. -G Ninja -DCMAKE_CUDA_COMPILER="$CMAKE_CUDA_COMPILER" -DCUDAToolkit_ROOT="$CUDAToolkit_ROOT"
+#
 #-DCMAKE_CUDA_ARCHITECTURES="all"
-
 #ninja.exe
 #ctest.exe
-
-
+#
